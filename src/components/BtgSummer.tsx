d8668cc0c4753b31031c7b1b4a39135c61abe491
Grégoire <72552360+Tuvedunyel@users.noreply.github.com>
2022-05-19 14:00:59 +0200

    ani changes

--- src/components/BtgSummer.tsx
+++ src/components/BtgSummer.tsx
@@ -2,7 +2,9 @@ import { FC } from "react";
 import "./sass/BtgSummer.scss";
 import SvgMorphe from "./SvgMorphe";
 import logo from "../img/logo-summer_4.gif";
+import orangeBlueBushLeaf from '../img/orangeBlue-bushLeaf.svg';
 import Jungle from "./Jungle";
+import orangeGreenLeaf from '../img/orangeGreen-leaf.svg';
 
 const BtgSummer: FC = () => {
     return (
@@ -10,6 +12,8 @@ const BtgSummer: FC = () => {
             <div className="btgSummer__container">
                 <SvgMorphe/>
                 <img src={ logo } alt="BTG Communication agence de communication à 360" className="logo"/>
+                <img src={ orangeBlueBushLeaf } alt="Plante verte et orange" className="planteVerteOrange"/>
+                <img src={ orangeGreenLeaf } alt="petite feuille orange" className="littleOrangeLeaf"/>
                 <Jungle  />
             </div>
         </div>
--- src/components/Jungle.tsx
+++ src/components/Jungle.tsx
@@ -1,13 +1,138 @@
-import React from 'react';
+import { useEffect } from 'react';
 import greenBush from '../img/green-bush.svg'
-import OrangeLeaf from "./jungleElements/OrangeLeaf";
+import blueBush from '../img/blue-bush.svg';
+import orangeRedBush from '../img/orangeRed-bush.svg';
+import whiteBush from '../img/white-bush.svg';
+
+
+import greenBlueLeaf from '../img/greenBlue-leaf.svg';
+import orangeBush from '../img/orange-bush.svg';
+import whiteLeaf from '../img/white-leaf.svg';
+import redLeaf from '../img/red-leaf.svg';
+import blueGreenLeaf from '../img/blueGreen-leaf.svg';
+import orangeLeaf from '../img/orange-leaf.svg';
+import redBush from '../img/red-bush.svg';
+import orangeRedBushLeft from '../img/leftbottom.svg';
+import anime from "animejs";
 
 const Jungle = () => {
+
+    useEffect( () => {
+        anime({
+            targets: '.OrangeBush',
+            keyframes: [
+                { rotate: -4, duration: 2000 },
+                { rotate: 4, duration: 1500 },
+                { rotate: -10, duration: 4000},
+                { rotate: 5, duration: 3500  },
+            ],
+            direction: 'alternate',
+            loop: true,
+            easing: 'linear',
+        })
+        anime({
+            targets: '.orangeRedBush',
+            keyframes: [
+                { rotate: -5, duration: 3000 },
+                { rotate: 5, duration: 3500 },
+                { rotate: 0, duration: 3000 },
+                { rotate: 10, duration: 4000 },
+                { rotate: 6, duration: 3000 },
+                { rotate: 15, duration: 4300 },
+                { rotate: 0, duration: 5000 },
+            ],
+            loop: true,
+            direction: 'alternate',
+            easing: "linear"
+        });
+        anime({
+            targets: '.BlueLeaf',
+            keyframes: [
+                { rotate: -3, duration: 3000 },
+                { rotate: 3, duration: 3500 },
+                { rotate: 0, duration: 2800 },
+                { rotate: 5, duration: 3500 },
+                { rotate: 3, duration: 2800 },
+                { rotate: 8, duration: 4000 },
+                { rotate: 3, duration: 3500 },
+                { rotate: 5, duration: 2500 },
+            ],
+            loop: true,
+            direction: 'alternate',
+            easing: "linear"
+        });
+        anime({
+            targets: '.redLeaf',
+            keyframes: [
+                { rotate: -5, duration: 3000 },
+                { rotate: 5, duration: 2500 },
+                { rotate: 3, duration: 2000 },
+                { rotate: 6, duration: 3500 },
+                { rotate: 3, duration: 2000 },
+                { rotate: 8, duration: 4000 },
+                { rotate: -5, duration: 5000 },
+            ],
+            loop: true,
+            direction: 'alternate',
+            easing: "linear"
+        });
+        anime({
+            targets: '.blueGreen',
+            keyframes: [
+                { rotate: -6, duration: 3000 },
+                { rotate: 6, duration: 3500 },
+                { rotate: 0, duration: 3000 },
+                { rotate: -10, duration: 4000 },
+                { rotate: 6, duration: 3000 },
+                { rotate: -15, duration: 4300 },
+                { rotate: 0, duration: 5000 },
+                { rotate: 6, duration: 2500 },
+                { rotate: -6, duration: 3000 }
+            ],
+            loop: true,
+            direction: 'alternate',
+            easing: "linear"
+        });
+        anime({
+            targets: '.redBush',
+            keyframes: [
+                { rotate: -5, duration: 3000 },
+                { rotate: 5, duration: 3500 },
+                { rotate: 0, duration: 3000 },
+                { rotate: 10, duration: 4000 },
+                { rotate: 6, duration: 3000 },
+                { rotate: 15, duration: 4300 },
+                { rotate: 0, duration: 5000 },
+                { rotate: 5, duration: 2500 },
+                { rotate: -5, duration: 3000 }
+            ],
+            loop: true,
+            direction: 'alternate',
+            easing: "linear"
+        })
+    }, [] );
+
+
     return (
         <div className="jungleContainer">
             <img src={ greenBush } alt="buisson vert"  className="greenBush" />
-            <OrangeLeaf />
-            {/*<img src={ blueBush } alt="Buisson vert"/>*/}
+            <img src={ orangeRedBushLeft } alt="Feuille orange" className="OrangeBush" />
+            <img src={ blueBush } alt="Buisson vert" className="blueBush"/>
+            <img src={ blueBush } alt="Buisson vert" className="blueBush second"/>
+            <img src={ orangeRedBush } alt="Feuille orange" className="orangeRedBush"/>
+            <img src={ whiteBush } alt="Feuille blanche" className="whiteBush"/>
+
+            <img src={ greenBlueLeaf } alt="petite feuille bleu" className="BlueLeaf"/>
+            <img src={ orangeBush } alt="buisson orange" className="orangeBush"/>
+            <img src={ whiteLeaf } alt="petite feuille blanche" className="whiteLeaf"/>
+            <img src={ blueBush } alt="Buisson vert" className="blueBush bottomRight"/>
+            <img src={ redLeaf } alt="Feuille rouge" className="redLeaf"/>
+            <img src={ whiteBush } alt="Feuille blanche" className="whiteBush topRight"/>
+            <img src={ blueGreenLeaf } alt="Feuille bleuée" className="blueGreen"/>
+            <img src={ whiteBush } alt="Feuille blanche" className="whiteBush topMiddle"/>
+            <img src={ orangeLeaf } alt="feuille orangée" className="orangeLeafImg"/>
+            <img src={ redBush } alt="Buisson rouge" className="redBush"/>
+            <img src={ whiteLeaf } alt="petite feuille blanche" className="whiteLeaf topLeft"/>
         </div>
     );
 };
